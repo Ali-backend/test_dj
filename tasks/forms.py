c0541ca613219500c3d2e5e3abb2936c9788b0c7
Ali-backend <mrsulvag@gmail.com>
2024-10-27 21:55:17 +0600

    test

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,12 +6,21 @@ class TaskForm(forms.ModelForm):
     category = forms.CharField
 
 
-        def clean(self):
-            cleaned_data = super().clean()
-            title = cleaned_data.get("title")
-            content = cleaned_data.get("content")
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get("title")
+        content = cleaned_data.get("content")
 
-            if (title and content) and (title.lower() != content.lower()):
-                raise forms.ValidationError('Заголовок не должен совподать')
-            else:
-                return cleaned_data
+        if (title and content) and (title.lower() != content.lower()):
+            raise forms.ValidationError('Заголовок не должен совподать')
+        else:
+            return cleaned_data
+
+
+    def clean_title(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get("title")
+
+        if title and title.lower() == 'python':
+            raise forms.ValidationError('слова python в загаловке нельзя использовать')
+        return title
